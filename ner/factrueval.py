2d5b1c31c59b56d5379df3ed21725a2795800317
Ivan Bondarenko <bond005@yandex.ru>
2024-04-13 01:11:04 +0700

    The FactRuEval parser implementation is finished.

--- ner/factrueval.py
+++ ner/factrueval.py
@@ -1,5 +1,5 @@
 import codecs
-import csv
+import copy
 import os
 from typing import Dict, List, Tuple
 
@@ -22,9 +22,13 @@ def load_spans(text: str, span_fname: str) -> Dict[int, Tuple[int, int]]:
                     span_id = -1
                 if span_id < 0:
                     raise ValueError(err_msg)
-                if span_id in res:
-                    raise ValueError(err_msg + f' The span ID = {span_id} is duplicated!')
-                span_text = ' '.join(parts_of_line[8:])
+                try:
+                    span_tokens_number = int(parts_of_line[5])
+                except:
+                    span_tokens_number = 0
+                if span_tokens_number < 1:
+                    raise ValueError(err_msg)
+                span_text = ' '.join(parts_of_line[(7 + span_tokens_number):])
                 if len(span_text) == 0:
                     raise ValueError(err_msg + ' The span text is empty!')
                 try:
@@ -50,21 +54,85 @@ def load_spans(text: str, span_fname: str) -> Dict[int, Tuple[int, int]]:
                 if subtext != span_text:
                     err_msg += f' The text {subtext} does not correspond to the text {span_text}'
                     raise ValueError(err_msg)
+                if span_id in res:
+                    if res[span_id] != (start_pos, start_pos + span_length):
+                        raise ValueError(err_msg + f' The span ID = {span_id} is duplicated!')
                 res[span_id] = (start_pos, start_pos + span_length)
             line_idx += 1
             curline = fp.readline()
     return res
 
 
-def find_entity_by_char_pos(char_pos: int, entities: List[Tuple[int, int]]) -> int:
-    if char_pos < 0:
-        return -1
-    found_idx = -1
-    for idx, (start_, end_) in enumerate(entities):
-        if (found_idx >= start_) and (found_idx < end_):
-            found_idx = idx
-            break
-    return found_idx
+def join_nested_entities(entities: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    if len(entities) == 0:
+        return []
+    if len(entities) == 1:
+        return entities
+    joined_entities = copy.copy(entities)
+    n = len(joined_entities)
+    is_changed = False
+    idx1 = 0
+    while idx1 < n:
+        overlapped = []
+        entity_start = joined_entities[idx1][0]
+        entity_end = joined_entities[idx1][1]
+        for idx2 in range(n):
+            if idx2 == idx1:
+                continue
+            if joined_entities[idx2][1] <= entity_start:
+                continue
+            if entity_end <= joined_entities[idx2][0]:
+                continue
+            overlapped.append(idx2)
+        if len(overlapped) > 0:
+            overlapped.sort(key=lambda it: (joined_entities[it][0], joined_entities[it][1]))
+            new_entity_start = min(entity_start, joined_entities[overlapped[0]][0])
+            new_entity_end = max(entity_end, joined_entities[overlapped[-1]][1])
+            removed = [joined_entities[it] for it in overlapped] + [joined_entities[idx1]]
+            for val in removed:
+                joined_entities.remove(val)
+                n -= 1
+            joined_entities.append((new_entity_start, new_entity_end))
+            idx1 = 0
+            del removed
+            is_changed = True
+        else:
+            idx1 += 1
+        del overlapped
+    joined_entities.sort(key=lambda it: (it[0], it[1]))
+    while is_changed:
+        n = len(joined_entities)
+        is_changed = False
+        idx1 = 0
+        while idx1 < n:
+            overlapped = []
+            entity_start = joined_entities[idx1][0]
+            entity_end = joined_entities[idx1][1]
+            for idx2 in range(n):
+                if idx2 == idx1:
+                    continue
+                if joined_entities[idx2][1] <= entity_start:
+                    continue
+                if entity_end <= joined_entities[idx2][0]:
+                    continue
+                overlapped.append(idx2)
+            if len(overlapped) > 0:
+                overlapped.sort(key=lambda it: (joined_entities[it][0], joined_entities[it][1]))
+                new_entity_start = min(entity_start, joined_entities[overlapped[0]][0])
+                new_entity_end = max(entity_end, joined_entities[overlapped[-1]][1])
+                removed = [joined_entities[it] for it in overlapped] + [joined_entities[idx1]]
+                for val in removed:
+                    joined_entities.remove(val)
+                    n -= 1
+                joined_entities.append((new_entity_start, new_entity_end))
+                idx1 = 0
+                del removed
+                is_changed = True
+            else:
+                idx1 += 1
+            del overlapped
+        joined_entities.sort(key=lambda it: (it[0], it[1]))
+    return joined_entities
 
 
 def load_named_entities(spans: Dict[int, Tuple[int, int]], ne_fname: str) -> Dict[str, List[Tuple[int, int]]]:
@@ -77,7 +145,7 @@ def load_named_entities(spans: Dict[int, Tuple[int, int]], ne_fname: str) -> Dic
             if len(prepline) > 0:
                 err_msg = f'{ne_fname}: line {line_idx} is wrong!'
                 parts_of_line = prepline.split()
-                if len(parts_of_line) < 6:
+                if len(parts_of_line) < 5:
                     raise ValueError(err_msg)
                 try:
                     entity_id = int(parts_of_line[0])
@@ -117,13 +185,10 @@ def load_named_entities(spans: Dict[int, Tuple[int, int]], ne_fname: str) -> Dic
                     spans_in_entity.add(span_id)
                 if entity_type not in res:
                     res[entity_type] = []
-                sorted_spans_in_entity = sorted(
-                    list(spans_in_entity),
-                    key=lambda it: (spans[it][0], spans[it][1])
-                )
-                entity_start = spans[sorted_spans_in_entity[0]][0]
-                entity_end = spans[sorted_spans_in_entity[-1]][1]
+                entity_start = min([spans[i][0] for i in spans_in_entity])
+                entity_end = max([spans[i][1] for i in spans_in_entity])
                 res[entity_type].append((entity_start, entity_end))
+                del spans_in_entity
             line_idx += 1
             curline = fp.readline()
     for entity_type in res:
@@ -131,8 +196,46 @@ def load_named_entities(spans: Dict[int, Tuple[int, int]], ne_fname: str) -> Dic
     return res
 
 
-def load_sample(dataset_path: str, item_name: str,
-                join_nested_entities: bool = True) -> Tuple[str, Dict[str, List[Tuple[int, int]]]]:
+def extend_entity_bounds(text: str, entity_start: int, entity_end: int) -> Tuple[int, int]:
+    is_changed = False
+    if entity_start > 0:
+        if text[entity_start - 1] == '«':
+            entity_start_ = entity_start - 1
+            is_changed = True
+        else:
+            entity_start_ = entity_start
+    else:
+        entity_start_ = entity_start
+    if entity_end < len(text):
+        if text[entity_end] == '»':
+            entity_end_ = entity_end + 1
+            is_changed = True
+        else:
+            entity_end_ = entity_end
+    else:
+        entity_end_ = entity_end
+    if not is_changed:
+        return entity_start, entity_end
+    n1 = 0
+    start_pos = entity_start_
+    found_idx = text[start_pos:entity_end_].find('«')
+    while found_idx >= 0:
+        n1 += 1
+        start_pos += (found_idx + 1)
+        found_idx = text[start_pos:entity_end_].find('«')
+    n2 = 0
+    start_pos = entity_start_
+    found_idx = text[start_pos:entity_end_].find('»')
+    while found_idx >= 0:
+        n2 += 1
+        start_pos += (found_idx + 1)
+        found_idx = text[start_pos:entity_end_].find('»')
+    if n1 == n2:
+        return entity_start_, entity_end_
+    return entity_start, entity_end
+
+
+def load_sample(dataset_path: str, item_name: str) -> Tuple[str, Dict[str, List[Tuple[int, int]]]]:
     if not os.path.isdir(dataset_path):
         raise ValueError(f'The directory "{dataset_path}" does not exist!')
     txt_fname = os.path.join(dataset_path, item_name + '.txt')
@@ -144,15 +247,13 @@ def load_sample(dataset_path: str, item_name: str,
         raise ValueError(f'The file "{span_fname}" does not exist!')
     if not os.path.isfile(ne_fname):
         raise ValueError(f'The file "{ne_fname}" does not exist!')
-    full_text = ''
-    with codecs.open(txt_fname, mode='r', encoding='utf-8', errors='ignore') as fp:
-        new_line = fp.readline()
-        while len(new_line) > 0:
-            full_text += new_line.replace('\r', '\n')
-            new_line = fp.readline()
+    with open(txt_fname, 'rb') as fp:
+        full_text = fp.read().decode('utf-8').replace('\r', '')
     if len(full_text.strip()) == 0:
         raise ValueError(f'The file "{txt_fname}" is empty!')
     full_text = full_text.rstrip()
     all_spans = load_spans(full_text, span_fname)
     entities_in_text = load_named_entities(all_spans, ne_fname)
+    for k in sorted(list(entities_in_text.keys())):
+        entities_in_text[k] = join_nested_entities([extend_entity_bounds(full_text, *it) for it in entities_in_text[k]])
     return full_text, entities_in_text
--- tests/test_ner.py
+++ tests/test_ner.py
@@ -4,11 +4,11 @@ import unittest
 
 try:
     from ner.ner import find_subphrase, find_entities_in_text
-    from ner.factrueval import load_sample
+    from ner.factrueval import load_sample, join_nested_entities, load_spans, extend_entity_bounds
 except:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     from ner.ner import find_subphrase, find_entities_in_text
-    from ner.factrueval import load_sample
+    from ner.factrueval import load_sample, join_nested_entities, load_spans, extend_entity_bounds
 
 
 class TestNER(unittest.TestCase):
@@ -240,7 +240,97 @@ class TestNER(unittest.TestCase):
 
 
 class TestFactRuEval(unittest.TestCase):
-    def test_load_sample_pos01(self):
+    def test_load_spans(self):
+        src_text = ('В понедельник 28 июня у здания мэрии Москвы на Тверской площади состоялась очередная '
+                    'несанкционированная акция протеста «День гнева», в этот раз направленная, главным образом, '
+                    'против политики московских и подмосковных властей. Среди требований, выдвигаемых организаторами '
+                    'акции: «немедленная отставка мэра Москвы Юрия Лужкова, расследование итогов его деятельности», '
+                    '«созыв московского общественного форума для обсуждения путей реформирования основных сфер '
+                    'жизнедеятельности в Москве», «восстановление прямых выборов глав регионов России», «роспуск '
+                    'нелегитимной Мосгордумы», отставка подмосковного губернатора Бориса Громова и др. Участникам '
+                    'акции предлагалось принести с собой лист бумаги или кусок ткани чёрного цвета, символизирующие '
+                    '«чёрную метку» для Юрия Лужкова.\n\nНачало акции было намечено на 19 часов; подчёркивалось, что '
+                    'она состоится несмотря на запрет властей. Освещающие акцию блоггеры сообщили, что автобусы с '
+                    'милицией стали занимать площадь у памятника Юрию Долгорукому ещё с 15 часов дня, центральная '
+                    'часть площади была огорожена. Ко времени начала акции вокруг огороженной территории собралось '
+                    'множество журналистов и прохожих, по мере прибытия самих участников акции милиция начала '
+                    'планомерно их задерживать и заталкивать в автобусы.\n\nВсего, по сообщениям блоггеров и СМИ, '
+                    'было задержано более 30 человек. Пользователь ЖЖ zyalt сообщает, что среди задержанных оказался '
+                    'депутат муниципального собрания района Отрадное, сопредседатель московского отделения движения '
+                    '«Солидарность» Михаил Вельмакин, известный правозащитник Лев Пономарев, координатор движения '
+                    '«Левый фронт» Сергей Удальцов.\n\nОрганизаторами акции выступили движение «За права человека», '
+                    'Союз координационных советов (СКС), институт «Коллективное действие», «Жилищная солидарность», '
+                    'Движение общежитий Москвы, Движение в защиту Химкинского леса, движение «Московский совет», '
+                    '«Координационный совет пострадавших соинвесторов».')
+        true_spans = {
+            22763: (37, 43),  # 6
+            22764: (31, 36),  # 5
+            22765: (47, 55),  # 8
+            22766: (56, 63),  # 7
+            22767: (313, 317),  # 4
+            22768: (318,  325),  # 7
+            22769: (306, 312),  # 6
+            22770: (477, 483),  # 6
+            22771: (531, 537),  # 6
+            22772: (562, 572),  # 10
+            22773: (610, 616),  # 6
+            22774: (617, 624),  # 7
+            22775: (756, 760),  # 4
+            22776: (761, 768),  # 7
+            22777: (301, 305),  # 4
+            22778: (598, 609),  # 11
+            22779: (584, 609),  # 25
+            68733: (584, 597),  # 13
+            22782: (968, 972),  # 4
+            22783: (973, 984),  # 11
+            22784: (958, 967),  # 9
+            22785: (1340, 1345),  # 5
+            22788: (1426, 1434),  # 8
+            22789: (1419, 1425),  # 6
+            22790: (1395, 1418),  # 23
+            22793: (1483, 1495),  # 12
+            22794: (1473, 1481),  # 8
+            22795: (1451, 1472),  # 21
+            22796: (1497, 1503),  # 6
+            22797: (1504, 1513),  # 9
+            22798: (1539, 1542),  # 3
+            22799: (1543, 1552),  # 9
+            22800: (1576, 1587),  # 11
+            22801: (1566, 1574),  # 8
+            22802: (1589, 1595),  # 6
+            22803: (1596, 1604),  # 8
+            22804: (1387, 1394),  # 7
+            22805: (1436, 1450),  # 14
+            22806: (1525, 1538),  # 13
+            22807: (1554, 1565),  # 11
+            22808: (1463, 1472),  # 9
+            22809: (1648, 1665),  # 17
+            22810: (1638, 1646),  # 8
+            22811: (1668, 1696),  # 28
+            22812: (1668, 1672),  # 4
+            22813: (1698, 1701),  # 3
+            22814: (1714, 1735),  # 21
+            22815: (1704, 1712),  # 8
+            22816: (1739, 1760),  # 21
+            22817: (1763, 1788),  # 25
+            22818: (1763, 1771),  # 8
+            22819: (1790, 1824),  # 34
+            22820: (1790, 1798),  # 8
+            22821: (1836, 1852),  # 16
+            22822: (1826, 1834),  # 8
+            22823: (1856, 1903),  # 47
+            22824: (1872, 1877),  # 5
+            22825: (1856, 1877),  # 21
+            38271: (1808, 1824),  # 16
+            38272: (1820, 1824),  # 4
+        }
+        loaded_spans = load_spans(src_text, os.path.join(os.path.dirname(__file__), 'testdata', 'book_100.spans'))
+        self.assertIsInstance(loaded_spans, dict)
+        self.assertEqual(set(loaded_spans.keys()), set(true_spans.keys()))
+        for k in sorted(list(true_spans.keys())):
+            self.assertEqual(loaded_spans[k], true_spans[k])
+
+    def test_load_sample(self):
         dataset_path = os.path.join(os.path.dirname(__file__), 'testdata')
         dataset_item = 'book_100'
         true_text = ('В понедельник 28 июня у здания мэрии Москвы на Тверской площади состоялась очередная '
@@ -269,10 +359,9 @@ class TestFactRuEval(unittest.TestCase):
                 (31, 43),
                 (562, 572),
                 (1395, 1434),
-                (1473, 1496),
                 (1451, 1496),
                 (1566, 1588),
-                (1647, 1666),
+                (1638, 1666),
                 (1668, 1696),
                 (1698, 1701),
                 (1704, 1736),
@@ -288,14 +377,94 @@ class TestFactRuEval(unittest.TestCase):
                 (306, 312),
                 (477, 483),
                 (531, 537),
+                (584, 597),
                 (958, 984),
+                (1419, 1434),
                 (1808, 1824)
             ],
             'person': [
-
+                (313,  325),
+                (610, 624),
+                (756, 768),
+                (968, 984),
+                (1340, 1345),
+                (1497, 1513),
+                (1539, 1552),
+                (1589, 1604)
             ]
         }
-        loaded_text, loaded_entities = load_sample(dataset_path, dataset_item)
+        res = load_sample(dataset_path, dataset_item)
+        self.assertIsInstance(res, tuple)
+        self.assertEqual(len(res), 2)
+        loaded_text, loaded_entities = res
+        self.assertIsInstance(loaded_text, str)
+        self.assertIsInstance(loaded_entities, dict)
+        self.assertEqual(loaded_text, true_text)
+        self.assertEqual(set(loaded_entities.keys()), set(true_entities.keys()))
+        for entity_type in sorted(list(true_entities.keys())):
+            self.assertIsInstance(loaded_entities[entity_type], list)
+            self.assertEqual(len(loaded_entities[entity_type]), len(true_entities[entity_type]))
+            for i, v in enumerate(loaded_entities[entity_type]):
+                self.assertIsInstance(v, tuple)
+                self.assertEqual(len(v), 2)
+                self.assertEqual(v, true_entities[entity_type][i])
+
+    def test_join_nested_entities_pos01(self):
+        src = [(0, 10), (10, 15), (25, 45)]
+        self.assertEqual(join_nested_entities(src), src)
+
+    def test_join_nested_entities_pos02(self):
+        src = [(0, 10)]
+        self.assertEqual(join_nested_entities(src), src)
+
+    def test_join_nested_entities_pos03(self):
+        src = []
+        self.assertEqual(join_nested_entities(src), src)
+
+    def test_join_nested_entities_pos04(self):
+        src = [(0, 10), (10, 15), (25, 45), (8, 11)]
+        tgt = [(0, 15), (25, 45)]
+        self.assertEqual(join_nested_entities(src), tgt)
+
+    def test_join_nested_entities_pos05(self):
+        src = [(0, 10), (10, 15), (25, 45), (8, 11), (14, 27)]
+        tgt = [(0, 45)]
+        self.assertEqual(join_nested_entities(src), tgt)
+
+    def test_join_nested_entities_pos09(self):
+        src = [(0, 10), (10, 15), (14, 48), (25, 45), (8, 11)]
+        tgt = [(0, 48)]
+        self.assertEqual(join_nested_entities(src), tgt)
+
+    def test_extend_entity_bounds_pos01(self):
+        s = 'координатор движения «Левый фронт» Сергей Удальцов'
+        src_bounds = (22, 33)
+        true_bounds = (21, 34)
+        self.assertEqual(extend_entity_bounds(s, *src_bounds), true_bounds)
+
+    def test_extend_entity_bounds_pos02(self):
+        s = 'координатор движения «Левый фронт» Сергей Удальцов'
+        src_bounds = (21, 33)
+        true_bounds = (21, 34)
+        self.assertEqual(extend_entity_bounds(s, *src_bounds), true_bounds)
+
+    def test_extend_entity_bounds_pos03(self):
+        s = 'координатор движения «Левый фронт» Сергей Удальцов'
+        src_bounds = (22, 34)
+        true_bounds = (21, 34)
+        self.assertEqual(extend_entity_bounds(s, *src_bounds), true_bounds)
+
+    def test_extend_entity_bounds_pos04(self):
+        s = 'координатор движения «Левый фронт» Сергей Удальцов'
+        src_bounds = (21, 34)
+        true_bounds = (21, 34)
+        self.assertEqual(extend_entity_bounds(s, *src_bounds), true_bounds)
+
+    def test_extend_entity_bounds_pos05(self):
+        s = 'координатор движения Левый фронт Сергей Удальцов'
+        src_bounds = (21, 32)
+        true_bounds = (21, 32)
+        self.assertEqual(extend_entity_bounds(s, *src_bounds), true_bounds)
 
 
 if __name__ == '__main__':
