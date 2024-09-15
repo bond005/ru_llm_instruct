7ad057b6e372f15d1e53dd3fc2f456e7e3a2e5a4
Ivan Bondarenko <bond005@yandex.ru>
2024-09-15 20:06:28 +0700

    RuStackoverflow dataset preparation is improved.

--- prepare_ru_stackoverflow_dataset.py
+++ prepare_ru_stackoverflow_dataset.py
@@ -1,14 +1,19 @@
 from argparse import ArgumentParser
 import codecs
-import json
+import csv
+import logging
 import os
 import random
+import sys
 
 from datasets import load_dataset
-from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from tqdm import tqdm
 
 
 RANDOM_SEED: int = 42
+ru_stackoverflow_logger = logging.getLogger(__name__)
 
 
 def main():
@@ -18,8 +23,18 @@ def main():
     parser.add_argument('-i', '--input', dest='input_name', type=str, required=True,
                         help='The input dataset name.')
     parser.add_argument('-o', '--output', dest='output_name', type=str, required=True,
-                        help='The output name of JSON file with structured RuStackoverflow.')
+                        help='The output name of CSV file with structured and verified RuStackoverflow.')
+    parser.add_argument('-m', '--model', dest='model_name', type=str, required=True,
+                        help='The DeepSeekCoder model name.')
     args = parser.parse_args()
+
+    input_dataset_name = os.path.normpath(args.input_name)
+    if not os.path.isdir(input_dataset_name):
+        raise IOError(f'The directory {input_dataset_name} does not exist!')
+
+    model_name = os.path.normpath(args.model_name)
+    if not os.path.isdir(model_name):
+        raise IOError(f'The directory {model_name} does not exist!')
 
     output_fname = os.path.normpath(args.output_name)
     if not os.path.isfile(output_fname):
@@ -52,57 +67,54 @@ def main():
                     )
             if len(variants_of_answer) > 0:
                 variants_of_answer.sort(key=lambda it: (-it['score'], -len(it['answer']), it['answer']))
-                variants_of_answer_without_duplicated = [variants_of_answer[0]]
-                for it in variants_of_answer[1:]:
-                    if it['score'] != variants_of_answer_without_duplicated[-1]['score']:
-                        variants_of_answer_without_duplicated.append(it)
                 questions_with_answers.append(
                     {
                         'question': question_text,
-                        'answers': variants_of_answer_without_duplicated
+                        'answer': variants_of_answer[0]['answer']
                     }
                 )
-                del variants_of_answer_without_duplicated
             del variants_of_answer
     print(f'There are {len(questions_with_answers)} samples in the prepared RuStackoverflow dataset.')
 
-    questions_with_many_answers = list(filter(lambda it: len(it['answers']) > 1, questions_with_answers))
-    print(f'There are {len(questions_with_many_answers)} questions with 2 or more answers.')
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
 
-    y = [(1 if len(it['answers']) > 1 else 0) for it in questions_with_answers]
-
-    X_train, X_test, y_train, y_test = train_test_split(questions_with_answers, y, test_size=0.1, stratify=y,
-                                                        random_state=RANDOM_SEED)
-
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=500, stratify=y_train,
-                                                      random_state=RANDOM_SEED)
-
-    result = {
-        'train': {
-            'question_and_answer': [
-                {'question': it['question'], 'answer': it['answers'][0]['answer']}
-                for it in X_train
-            ],
-            'question_and_many_answers': list(filter(lambda it: len(it['answers']) > 1, X_train))
-        },
-        'validation': {
-            'question_and_answer': [
-                {'question': it['question'], 'answer': it['answers'][0]['answer']}
-                for it in X_val
-            ],
-            'question_and_many_answers': list(filter(lambda it: len(it['answers']) > 1, X_val))
-        },
-        'test': {
-            'question_and_answer': [
-                {'question': it['question'], 'answer': it['answers'][0]['answer']}
-                for it in X_test
-            ],
-            'question_and_many_answers': list(filter(lambda it: len(it['answers']) > 1, X_test))
-        }
-    }
-    with codecs.open(output_fname, mode='w', encoding='utf-8') as fp:
-        json.dump(obj=result, fp=fp, ensure_ascii=False, indent=4)
+    prompt = ('You are a programming specialist who also knows Russian. '
+              'Please read the question and the answer to this question. '
+              'Then think carefully and tell me if this answer is correct, '
+              'complete enough and as clear as possible an answer to the question asked? '
+              'Just write "yes" or "no", that\'s all.\n\n\n'
+              'The question:\n\n\n{question}\n\n\nThe answer to this question:\n\n\n{answer}\n\n\n'
+              'Your verdict on the correctness, completeness and clarity of the answer to the question '
+              '(only "yes" or "no"): ')
+    with codecs.open(output_fname, mode='w', encoding='utf-8', errors='ignore', buffering=0) as fp:
+        data_writer = csv.writer(fp, delimiter=',', quotechar='"')
+        data_writer.writerow(['QUESTION', 'ANSWER', 'VERIFICATION'])
+        for sample in tqdm(questions_with_answers):
+            messages = [
+                {
+                    'role': 'user',
+                    'content': prompt.format(question=sample['question'], answer=sample['answer'])
+                }
+            ]
+            inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True,
+                                                   return_tensors='pt').to(model.device)
+            outputs = model.generate(inputs, max_new_tokens=10, do_sample=False, top_k=50, top_p=0.95,
+                                     num_return_sequences=1, eos_token_id=tokenizer.eos_token_id,
+                                     pad_token_id=tokenizer.eos_token_id)
+            result = ' '.join(tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True).strip().split())
+            data_writer.writerow([sample['question'], sample['answer'], result])
 
 
 if __name__ == '__main__':
+    ru_stackoverflow_logger.setLevel(logging.INFO)
+    fmt_str = '%(filename)s[LINE:%(lineno)d]# %(levelname)-8s ' \
+              '[%(asctime)s]  %(message)s'
+    formatter = logging.Formatter(fmt_str)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    ru_stackoverflow_logger.addHandler(stdout_handler)
+    file_handler = logging.FileHandler('ru_stackoverflow_filter.log')
+    file_handler.setFormatter(formatter)
+    ru_stackoverflow_logger.addHandler(file_handler)
     main()
