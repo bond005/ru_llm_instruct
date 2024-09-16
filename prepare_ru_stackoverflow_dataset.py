67738ce9f4b62542bfbfedd67f420efa05a774e9
Ivan Bondarenko <bond005@yandex.ru>
2024-09-16 22:02:31 +0700

    Some bugs in RuStackoverflow preparing are fixed.

--- prepare_ru_stackoverflow_dataset.py
+++ prepare_ru_stackoverflow_dataset.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 import codecs
-import csv
 import gc
+import json
 import logging
 import os
 import random
@@ -24,12 +24,21 @@ def main():
     parser.add_argument('-i', '--input', dest='input_name', type=str, required=True,
                         help='The input dataset name.')
     parser.add_argument('-o', '--output', dest='output_name', type=str, required=True,
-                        help='The output name of CSV file with structured and verified RuStackoverflow.')
+                        help='The output name of JSONL file with structured and verified RuStackoverflow.')
     parser.add_argument('-m', '--model', dest='model_name', type=str, required=True,
                         help='The DeepSeekCoder model name.')
+    parser.add_argument('-n', '--nrows', dest='number_of_rows', type=int, required=False, default=None,
+                        help='The maximal number of selected rows.')
     parser.add_argument('--maxlen', dest='maxlen', type=int, required=False, default=None,
                         help='The maximal length of the prompt.')
     args = parser.parse_args()
+
+    if args.number_of_rows is not None:
+        if args.number_of_rows < 2:
+            err_msg = (f'The maximal number of selected rows is too small! '
+                       f'Expected 2 or greater, got {args.number_of_rows}.')
+            ru_stackoverflow_logger.error(err_msg)
+            raise ValueError(err_msg)
 
     input_dataset_name = os.path.normpath(args.input_name)
     if not os.path.isdir(input_dataset_name):
@@ -57,6 +66,11 @@ def main():
                for it in filtered_dataset['answers']]
     del dataset, filtered_dataset
 
+    indices = list(range(len(questions)))
+    if args.number_of_rows is not None:
+        if (args.number_of_rows * 5) < len(indices):
+            indices = random.sample(population=indices, k=args.number_of_rows * 5)
+
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     prompt = ('You are a programming specialist who also knows Russian. '
               'Please read the question and the answer to this question. '
@@ -67,7 +81,9 @@ def main():
               'Your verdict on the correctness, completeness and clarity of the answer to the question '
               '(only "yes" or "no"): ')
     max_seq_len = 0
-    for question_text, answers_on_question in tqdm(zip(questions, answers), total=len(questions)):
+    for sample_idx in tqdm(indices):
+        question_text = questions[sample_idx]
+        answers_on_question = answers[sample_idx]
         if len(question_text) > 0:
             variants_of_answer = []
             for answer_text, answer_score in zip(answers_on_question[0], answers_on_question[1]):
@@ -115,22 +131,27 @@ def main():
     print(f'There are {len(questions_with_answers)} samples in the prepared RuStackoverflow dataset.')
     print(f'The maximal prompt length is {max_seq_len}.')
 
+    random.shuffle(questions_with_answers)
+    if args.number_of_rows is not None:
+        if args.number_of_rows < len(questions_with_answers):
+            questions_with_answers = questions_with_answers[:args.number_of_rows]
+
     model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
 
 
     with codecs.open(output_fname, mode='w', encoding='utf-8', errors='ignore', buffering=0) as fp:
-        data_writer = csv.writer(fp, delimiter=',', quotechar='"')
-        data_writer.writerow(['QUESTION', 'ANSWER', 'VERIFICATION'])
         for sample in tqdm(questions_with_answers):
-            inputs = tokenizer.apply_chat_template(sample['prompt_for_verification'],
-                                                   return_tensors='pt').to(model.device)
-            seqlen = len(inputs[0])
-            outputs = model.generate(inputs, max_new_tokens=5, do_sample=False, top_k=50, top_p=0.95,
+            inputs_ids = tokenizer(sample['prompt_for_verification'], return_tensors='pt').input_ids.to(model.device)
+            seqlen = len(inputs_ids[0])
+            outputs = model.generate(input_ids=inputs_ids, max_new_tokens=5, do_sample=False, top_k=50, top_p=0.95,
                                      num_return_sequences=1, eos_token_id=tokenizer.eos_token_id,
                                      pad_token_id=tokenizer.eos_token_id)
             result = ' '.join(tokenizer.decode(outputs[0][seqlen:], skip_special_tokens=True).strip().split())
-            data_writer.writerow([sample['question'], sample['answer'], result])
-            del outputs, result, inputs
+            fp.write(json.dumps(
+                {'query': sample['question'], 'response': sample['answer'], 'verification': result},
+                ensure_ascii=False
+            ) + '\n')
+            del outputs, result, inputs_ids
             gc.collect()
             torch.cuda.empty_cache()
 
