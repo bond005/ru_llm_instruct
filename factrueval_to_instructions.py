ef8df21ef64fa6e9600516e8c180f0587ec12535
Ivan Bondarenko <bond005@yandex.ru>
2024-11-04 16:22:24 +0700

    Data loading from FactRuEval-2016 is modified.

--- factrueval_to_instructions.py
+++ factrueval_to_instructions.py
@@ -8,6 +8,46 @@ import warnings
 from ner.factrueval import load_sample
 
 
+INSTRUCTIONS_FOR_ORGANIZATIONS = [
+    'Найди, пожалуйста, все именованные сущности типа "Организация" в следующем тексте и '
+    'выпиши список таких сущностей.',  # 0
+    'Прочитай следующий текст и выпиши список всех сущностей класса "Организация".',  # 1
+    'Распознай именованные сущности типа "Организация" в тексте и напиши их список '
+    '(в каждой строке - отдельная сущность).',  # 2
+    'Определи все организации, упоминаемые в тексте, и перечисли их в виде списка.',  # 3
+    'Какие организации упоминаются в этом тексте?',  # 4
+    'Что за фирмы, учреждения и прочие организации есть в этом тексте? Выпиши их перечень, пожалуйста.',  # 5
+    'Найди, пожалуйста, все именованные сущности класса "Организация" в заданном тексте и '
+    'напиши перечень найденных сущностей (каждая - с новой строки).',  # 6
+]
+INSTRUCTIONS_FOR_PERSONS = [
+    'Найди, пожалуйста, все именованные сущности типа "Человек" в следующем тексте и '
+    'выпиши список таких сущностей.',  # 0
+    'Прочитай следующий текст и выпиши список всех сущностей класса "Человек".',  # 1
+    'Распознай именованные сущности типа "Человек" в тексте и напиши их список '
+    '(в каждой строке - отдельная сущность).',  # 2
+    'Определи имена всех людей, упоминаемых в тексте, и перечисли их в виде списка.',  # 3
+    'Имена (фамилии и, может быть, отчества) каких людей упоминаются в этом тексте?',  # 4
+    'Фамилии и имена (и, возможно, отчества) каких людей обсуждаются в этом тексте? '
+    'Выпиши их перечень, пожалуйста.',  # 5
+    'Найди, пожалуйста, все именованные сущности класса "Человек" в заданном тексте и '
+    'напиши перечень найденных сущностей (каждая - с новой строки).',  # 6
+]
+INSTRUCTIONS_FOR_LOCATIONS = [
+    'Найди, пожалуйста, все именованные сущности типа "Местоположение" в следующем тексте и '
+    'выпиши список таких сущностей.',  # 0
+    'Прочитай следующий текст и выпиши список всех сущностей класса "Локация".',  # 1
+    'Распознай именованные сущности типа "Местоположение" в тексте и напиши их список '
+    '(в каждой строке - отдельная сущность).',  # 2
+    'Определи все локации, упоминаемые в тексте, и перечисли их в виде списка.',  # 3
+    'Какие географические места упоминаются в этом тексте?',  # 4
+    'Что за места и локации приведены в этом тексте? Выпиши их перечень, пожалуйста.',  # 5
+    'Найди, пожалуйста, все именованные сущности класса "Местоположение" в заданном тексте и '
+    'напиши перечень найденных сущностей (каждая - с новой строки).',  # 6
+]
+NEGATIVE_ANSWER = 'В этом тексте нет именованных сущностей такого типа.'
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('-s', '--src', dest='source_factrueval_name', type=str, required=True,
@@ -49,6 +89,8 @@ def main():
     loaded = dict()
     set_of_entities = set()
     for sample in sorted(factrueval_samples):
+        if sample in loaded:
+            raise ValueError(f'The sample {sample} is duplicated!')
         try:
             loaded[sample] = load_sample(input_dirname, sample)
         except Exception as err:
@@ -84,95 +126,119 @@ def main():
     if restarts > max_restarts:
         raise ValueError(f'The data loaded from "{input_dirname}" cannot be splitted by train and test.')
 
+    samples_for_training = factrueval_samples[0:n_training_samples]
+    samples_for_validation = factrueval_samples[n_training_samples:]
+    del factrueval_samples
+    samples_for_training.sort(key=lambda it: int(it[5:]))
+    samples_for_validation.sort(key=lambda it: int(it[5:]))
+
     with codecs.open(output_csv_fname_for_training, mode='w', encoding='utf-8') as fp:
         data_writer = csv.writer(fp, delimiter=',', quotechar='"')
-        data_writer.writerow(['input', 'target'])
-        for train_sample in factrueval_samples[0:n_training_samples]:
+        data_writer.writerow(['instruction', 'context', 'target'])
+        for train_sample in samples_for_training:
             text, entities = loaded[train_sample]
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Организация" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+            instruction = random.choice(INSTRUCTIONS_FOR_ORGANIZATIONS)
             if 'organization' in entities:
                 if len(entities['organization']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no organizations in the {train_sample}')
                 else:
                     true_answer = f'{text[entities["organization"][0][0]:entities["organization"][0][1]]}'
                     for ent_start, ent_end in entities['organization'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Местоположение" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            if true_answer == NEGATIVE_ANSWER:
+                if random.random() > 0.5:
+                    data_writer.writerow([instruction, text, true_answer])
+            else:
+                data_writer.writerow([instruction, text, true_answer])
+            del true_answer
+            instruction = random.choice(INSTRUCTIONS_FOR_LOCATIONS)
             if 'location' in entities:
                 if len(entities['location']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no locations in the {train_sample}')
                 else:
                     true_answer = f'{text[entities["location"][0][0]:entities["location"][0][1]]}'
                     for ent_start, ent_end in entities['location'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Человек" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            if true_answer == NEGATIVE_ANSWER:
+                if random.random() > 0.5:
+                    data_writer.writerow([instruction, text, true_answer])
+            else:
+                data_writer.writerow([instruction, text, true_answer])
+            del true_answer
+            instruction = random.choice(INSTRUCTIONS_FOR_PERSONS)
             if 'person' in entities:
                 if len(entities['person']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no persons in the {train_sample}')
                 else:
                     true_answer = f'{text[entities["person"][0][0]:entities["person"][0][1]]}'
                     for ent_start, ent_end in entities['person'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            if true_answer == NEGATIVE_ANSWER:
+                if random.random() > 0.5:
+                    data_writer.writerow([instruction, text, true_answer])
+            else:
+                data_writer.writerow([instruction, text, true_answer])
+            del text, entities, true_answer
 
     with codecs.open(output_csv_fname_for_validation, mode='w', encoding='utf-8') as fp:
         data_writer = csv.writer(fp, delimiter=',', quotechar='"')
         data_writer.writerow(['input', 'target'])
-        for val_sample in factrueval_samples[n_training_samples:]:
+        for val_sample in samples_for_validation:
             text, entities = loaded[val_sample]
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Организация" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+            instruction = INSTRUCTIONS_FOR_ORGANIZATIONS[0]
             if 'organization' in entities:
                 if len(entities['organization']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no organizations in the {val_sample}')
                 else:
                     true_answer = f'{text[entities["organization"][0][0]:entities["organization"][0][1]]}'
                     for ent_start, ent_end in entities['organization'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Местоположение" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            data_writer.writerow([instruction, text, true_answer])
+            del true_answer
+            instruction = INSTRUCTIONS_FOR_LOCATIONS[0]
             if 'location' in entities:
                 if len(entities['location']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no locations in the {val_sample}')
                 else:
                     true_answer = f'{text[entities["location"][0][0]:entities["location"][0][1]]}'
                     for ent_start, ent_end in entities['location'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
-            input_prompt = (f'<LM>Найди, пожалуйста, все именованные сущности типа "Человек" в следующем тексте '
-                            f'и выпиши список таких сущностей. {text}')
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            data_writer.writerow([instruction, text, true_answer])
+            del true_answer
+            instruction = INSTRUCTIONS_FOR_PERSONS[0]
             if 'person' in entities:
                 if len(entities['person']) == 0:
-                    true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
+                    true_answer = NEGATIVE_ANSWER
+                    warnings.warn(f'There are no persons in the {val_sample}')
                 else:
                     true_answer = f'{text[entities["person"][0][0]:entities["person"][0][1]]}'
                     for ent_start, ent_end in entities['person'][1:]:
                         true_answer += f'\n{text[ent_start:ent_end]}'
-                    true_answer += '</s>'
             else:
-                true_answer = 'В этом тексте нет именованных сущностей такого типа.</s>'
-            data_writer.writerow([input_prompt, true_answer])
+                true_answer = NEGATIVE_ANSWER
+                warnings.warn(f'There are no persons in the {train_sample}')
+            data_writer.writerow([instruction, text, true_answer])
+            del true_answer
 
 
 if __name__ == '__main__':
